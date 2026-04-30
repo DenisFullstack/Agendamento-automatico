6502f4964a8e6beebb81a8a2d625c28493808abe
Denis.Fullstack <denferbar1@gmail.com>
2026-04-30 18:52:36 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+
+    /* ---------------- FIREBASE INIT E DATABASE ---------------- */
+    const firebaseConfig = {
+    apiKey: "AIzaSyCdk70YNlUvB1uU5UlFNAhGRdZgP8eUIsM",
+    authDomain: "cureprototipo.firebaseapp.com",
+    projectId: "cureprototipo",
+    storageBucket: "cureprototipo.firebasestorage.app",
+    messagingSenderId: "954641507266",
+    appId: "1:954641507266:web:11d57cf2af3da58c446c5b",
+    };
+
+const ADMIN_CREDENTIALS = {
+    email: 'MANICURE@gmail.com',
+    senha: 'MANICURE',
+    whatsapp: '5519000000000'
+};
